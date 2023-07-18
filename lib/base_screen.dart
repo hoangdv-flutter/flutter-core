ada894b4afdd85faf44db1c92b06d3e18fb219f6
Hoangkute123xyz <hoangdv@wavez.vn>
2023-07-18 12:29:52 +0700

    update

--- lib/base_screen.dart
+++ lib/base_screen.dart
@@ -2,6 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:flutter_core/ext/context.dart';
 import 'package:flutter_core/util/crash_log.dart';
 
+
+abstract class BaseScreenState<S extends StatefulWidget> extends State<S> {
+  Future<bool> onBackPressed(BuildContext context) async {
+    // if (ModalRoute.of(context)?.settings.name == runtimeType.toString()) {
+    //   context.popScreen();
+    //   return false;
+    // }
+    context.popScreen();
+    return false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    CrashlyticsLogger.logError(runtimeType.toString());
+    return WillPopScope(
+        onWillPop: () {
+          return onBackPressed(context);
+        },
+        child: onBuild(context));
+  }
+
+  Widget onBuild(BuildContext context);
+}
+
 abstract class BaseScreen extends StatelessWidget {
   const BaseScreen({super.key});
 
