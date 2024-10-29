c58dd6fa2368fa86b8cdd9b7464b55efff2b33d0
Hoang <hoangdv@wavez.vn>
2024-10-29 10:03:49 +0700

    update

--- lib/di/package_di.config.dart
+++ lib/di/package_di.config.dart
@@ -8,44 +8,44 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:flutter_core/data/shared/premium_holder.dart' as _i932;
-import 'package:flutter_core/di/app_module.dart' as _i79;
-import 'package:flutter_core/util/util.dart' as _i763;
-import 'package:get_it/get_it.dart' as _i174;
-import 'package:injectable/injectable.dart' as _i526;
-import 'package:package_info_plus/package_info_plus.dart' as _i655;
-import 'package:shared_preferences/shared_preferences.dart' as _i460;
+import 'package:flutter_core/data/shared/premium_holder.dart' as _i6;
+import 'package:flutter_core/di/app_module.dart' as _i7;
+import 'package:flutter_core/util/util.dart' as _i5;
+import 'package:get_it/get_it.dart' as _i1;
+import 'package:injectable/injectable.dart' as _i2;
+import 'package:package_info_plus/package_info_plus.dart' as _i4;
+import 'package:shared_preferences/shared_preferences.dart' as _i3;
 
-extension GetItInjectableX on _i174.GetIt {
+extension GetItInjectableX on _i1.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
-  Future<_i174.GetIt> init({
+  Future<_i1.GetIt> init({
     String? environment,
-    _i526.EnvironmentFilter? environmentFilter,
+    _i2.EnvironmentFilter? environmentFilter,
   }) async {
-    final gh = _i526.GetItHelper(
+    final gh = _i2.GetItHelper(
       this,
       environment,
       environmentFilter,
     );
     final appModule = _$AppModule();
-    await gh.singletonAsync<_i460.SharedPreferences>(
+    await gh.singletonAsync<_i3.SharedPreferences>(
       () => appModule.sharedPref,
       preResolve: true,
     );
-    await gh.singletonAsync<_i655.PackageInfo>(
+    await gh.singletonAsync<_i4.PackageInfo>(
       () => appModule.packageInfo,
       preResolve: true,
     );
-    gh.singleton<_i763.NetWorkChecker>(
-      () => _i763.NetWorkChecker(),
+    gh.singleton<_i5.NetWorkChecker>(
+      () => _i5.NetWorkChecker(),
       dispose: (i) => i.dispose(),
     );
-    gh.singleton<_i932.PremiumHolder>(
-      () => _i932.PremiumHolder(gh<_i460.SharedPreferences>()),
+    gh.singleton<_i6.PremiumHolder>(
+      () => _i6.PremiumHolder(gh<_i3.SharedPreferences>()),
       dispose: (i) => i.disposed(),
     );
     return this;
   }
 }
 
-class _$AppModule extends _i79.AppModule {}
+class _$AppModule extends _i7.AppModule {}
